f841664c6b2151c949d2fe4d28e00eff6724e522
JonathanTheZero <jonathan.frohne@web.de>
2024-10-11 12:20:26 +0800

    FSR

---
+++ Drafts/FSR/FSR.ino
@@ -0,0 +1,33 @@
+#define FORCE_SENSOR_PIN 35
+
+void setup() {
+  Serial.begin(9600);
+
+  // set the ADC attenuation to 11 dB (up to ~3.3V input)
+  analogSetAttenuation(ADC_11db);
+}
+
+void loop() {
+  int analogReading = analogRead(FORCE_SENSOR_PIN);
+
+  if (analogReading == 0) {
+    return;
+  }
+
+  Serial.print("The force sensor value = ");
+  Serial.print(analogReading);  // print the raw analog reading
+
+  if (analogReading < 10) {  // from 0 to 9
+    Serial.println(" -> no pressure");
+  } else if (analogReading < 200) {  // from 10 to 199
+    Serial.println(" -> light touch");
+  } else if (analogReading < 500) {  // from 200 to 499
+    Serial.println(" -> light squeeze");
+  } else if (analogReading < 800) {  // from 500 to 799
+    Serial.println(" -> medium squeeze");
+  } else {  // from 800 to 1023
+    Serial.println(" -> big squeeze");
+  }
+
+  delay(1000);
+}

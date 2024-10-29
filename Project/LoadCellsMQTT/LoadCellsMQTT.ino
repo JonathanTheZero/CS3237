ecc474d4ba3f6fefee4f1a1fc0849f87be0e458a
JonathanTheZero <jonathan.frohne@web.de>
2024-10-29 11:33:13 +0800

    Added MQTT to Multi FSR

---
+++ Project/LoadCellsMQTT/LoadCellsMQTT.ino
@@ -0,0 +1,117 @@
+#include "HX711.h"
+#include "Arduino.h"
+#include <WiFi.h>
+#include "ESP32MQTTClient.h"
+const char *ssid = "DESKTOP-496OVRO";
+const char *pass = "9Wz7344&";
+
+char *server = "mqtt://192.168.137.1:1883";
+
+char *publishTopic[2] = { "esp32/0/load_cells/0", "esp32/0/load_cells/1" };
+
+ESP32MQTTClient mqttClient;
+
+HX711 scale0;
+HX711 scale1;
+
+HX711 scales[2] = { scale0, scale1 };
+
+//uint8_t dataPin = 6;
+//uint8_t clockPin = 7;
+uint8_t dataPin[2] = { 26, 13 };   //for esp32
+uint8_t clockPin[2] = { 25, 12 };  //for esp32
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(HX711_LIB_VERSION);
+  Serial.println();
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.println();
+
+  WiFi.setHostname("load_cells");
+
+  mqttClient.enableDebuggingMessages();
+
+  mqttClient.setURI(server);
+  mqttClient.enableLastWillMessage("lwt", "I am going offline");
+  mqttClient.setKeepAlive(30);
+  mqttClient.loopStart();
+
+  for (int i = 0; i < 2; i++) {
+    scales[i].begin(dataPin[i], clockPin[i]);
+
+    Serial.print("UNITS ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(scales[i].get_units(20));
+  }
+
+  Serial.println("\nEmpty the scale, press a key to continue");
+  while (!Serial.available())
+    ;
+  while (Serial.available()) Serial.read();
+
+  for (int i = 0; i < 2; i++) {
+    scales[i].tare(20);
+    Serial.print("UNITS ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(scales[i].get_units(20));
+  }
+
+  Serial.println("\nPut 1000 gram in the scale, press a key to continue");
+  while (!Serial.available())
+    ;
+  while (Serial.available()) Serial.read();
+
+  for (int i = 0; i < 2; i++) {
+    scales[i].calibrate_scale(1700, 20);
+    Serial.print("UNITS ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(scales[i].get_units(20));
+  }
+
+  Serial.println("\nScale is calibrated, press a key to continue");
+  // Serial.println(scale.get_scale());
+  // Serial.println(scale.get_offset());
+  while (!Serial.available())
+    ;
+  while (Serial.available()) Serial.read();
+}
+
+
+void loop() {
+  for (int i = 0; i < 2; i++) {
+    Serial.print("UNITS ");
+    Serial.print(i);
+    Serial.print(": ");
+
+    float reading = scales[i].get_units(20);
+    Serial.println(reading);
+
+    String msg = String(i) + " " + String(reading);
+    mqttClient.publish(publishTopic[i], msg, 0, false);
+  }
+  delay(250);
+}
+
+void onMqttConnect(esp_mqtt_client_handle_t client) {
+  if (mqttClient.isMyTurn(client))  // can be omitted if only one client
+  {
+  }
+}
+
+void handleMQTT(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+  auto *event = static_cast<esp_mqtt_event_handle_t>(event_data);
+  mqttClient.onEventCallback(event);
+}
--- Project/Multi_FSR/Multi_FSR.ino
+++ Project/Multi_FSR/Multi_FSR.ino
@@ -1,6 +1,9 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
+#include "Arduino.h"
+#include <WiFi.h>
+#include "ESP32MQTTClient.h"
 
 
 #define FORCE_SENSOR_PIN_1 35
@@ -8,12 +11,25 @@
 #define FORCE_SENSOR_PIN_3 33
 #define FORCE_SENSOR_PIN_4 34
 #define READING_THRESHOLD 10
-#define AUDIO_PLAYING_VALUE 513
+#define AUDIO_PLAYING_VALUE -1
+
 
 #define SPEAKER_RX_PORT 27
 #define SPEAKER_TX_PORT 26
 SoftwareSerial speakerSoftwareSerial(SPEAKER_RX_PORT, SPEAKER_TX_PORT);
 DFRobotDFPlayerMini DFAudioPlayer;
+
+
+const char *ssid = "DESKTOP-496OVRO";
+const char *pass = "9Wz7344&"; //temp password
+char *server = "mqtt://192.168.137.1:1883";
+char *publishTopic[4] = {
+  ("esp32/1/fsrs/0"),
+  ("esp32/1/fsrs/1"),
+  ("esp32/1/fsrs/2"),
+  ("esp32/1/fsrs/3")
+};
+ESP32MQTTClient mqttClient;
 
 
 void setup() {
@@ -23,12 +39,56 @@ void setup() {
   // set the ADC attenuation to 11 dB (up to ~3.3V input)
   analogSetAttenuation(ADC_11db);
   initialise_dfplayer();
+  initialise_mqtt();
 }
 
 
 void loop() {
   read_values_and_act();
   delay(1000);
+}
+
+
+
+void initialise_mqtt() {
+  log_i();
+  log_i("setup, ESP.getSdkVersion(): ");
+  log_i("%s", ESP.getSdkVersion());
+
+
+  mqttClient.enableDebuggingMessages();
+  mqttClient.setURI(server);
+  mqttClient.enableLastWillMessage("lwt", "I am going offline");
+  mqttClient.setKeepAlive(30);
+  WiFi.begin(ssid, pass);
+  WiFi.setHostname("ESP32 speaker client");
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(100);
+  }
+  Serial.println();
+  mqttClient.loopStart();
+}
+
+
+void onMqttConnect(esp_mqtt_client_handle_t client) {
+  Serial.println(F("[MQTT] Connected to the MQTT broker and subscribing to topics..."));
+
+  // mqttClient.subscribe(trackTopic, [](const String &payload) {
+  //   Serial.print(F("[MQTT] Received: "));
+  //   Serial.println(String(trackTopic) + String(" ") + String(payload.c_str()));
+
+  //   int value = payload.toInt();
+  //   play_track(value);
+  // });
+}
+
+
+void handleMQTT(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+  auto *event = static_cast<esp_mqtt_event_handle_t>(event_data);
+  mqttClient.onEventCallback(event);
 }
 
 
@@ -62,6 +122,8 @@ void read_values_and_act() {
     } else {
       Serial.println(F(" -> big squeeze"));
     }
+    String msg = String(i) + F(": ") + String(readings[i]);
+    mqttClient.publish(publishTopic[i], msg, 0, false);
   }
 
   if (readings[0] >= 1000
@@ -99,7 +161,8 @@ void play_track(int trackID) {
   }
 
   // Check if the player is already playing
-  if (DFAudioPlayer.readState() == AUDIO_PLAYING_VALUE) {
+  int state = DFAudioPlayer.readState();
+  if (state == AUDIO_PLAYING_VALUE) {
     Serial.println(F("[DFPlayer] A track is already playing. Skipping play request."));
     delay(250);
     return;
@@ -108,6 +171,7 @@ void play_track(int trackID) {
   Serial.print(F("[DFPlayer] Playing track with ID "));
   Serial.println(trackID);
   DFAudioPlayer.play(trackID);
+  delay(100);
 }
 
 

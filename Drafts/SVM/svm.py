6b7e8c571be848348e65fb6c55e434b8981336dc
JonathanTheZero <jonathan.frohne@web.de>
2024-11-01 16:37:53 +0800

    SVM works

--- Drafts/SVM/svm.py
+++ Drafts/SVM/svm.py
@@ -1,33 +1,29 @@
 import numpy as np
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-
-
-import os
-import numpy as np
-from sklearn import svm
+import pandas as pd
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import joblib
+import os
 
 
 MODEL_FILEPATH = "./svm_model.pk1"
-FEATURES_FILEPATH = "./iot_features.npy"
-LABELS_FILEPATH = "iot_labels.npy"
+DATA_FILEPATH = "./training_data.csv"
 
 
 if __name__ == "__main__":
-    X = np.load(FEATURES_FILEPATH)
-    y = np.load(LABELS_FILEPATH)
+    data = pd.read_csv(DATA_FILEPATH)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    X = data.iloc[:, :-1].values
+    y = data.iloc[:, -1].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42
+    )
 
     if os.path.exists(MODEL_FILEPATH):
-        model: svm.SVC = joblib.load(MODEL_FILEPATH)
+        model: SVC = joblib.load(MODEL_FILEPATH)
         print("Model loaded from file.")
     else:
-        model = svm.SVC()
+        model = SVC()
         model.fit(X_train, y_train)
         print("Model trained and saved to file.")
         joblib.dump(model, MODEL_FILEPATH)
